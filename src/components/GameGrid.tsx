669bbb0b7bcb24581f85912873294be8be80a51d
ramazanim <ramazanim@berea.edu>
2024-03-08 16:26:34 -0500

    cleaned up game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -17,7 +17,7 @@ function GameGrid({ gameQuery }: Props) {
       {error && <Text>Network Error</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={10}
+        spacing={6}
         padding="10px"
       >
         {isLoading &&
